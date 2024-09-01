74fd2a7c7c9866bcc9f8485870293ad71f05c63e
Arseniy Medyakov <medyakov.senya@bk.ru>
2024-09-01 13:50:15 +0300

    corrected sypress tests

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -28,68 +28,80 @@ describe('constructor', () => {
     });
   });
 
-  let i = 0;
-
   it('ingredient added to the constructor', () => {
+    cy.get(selectedIngredients).should(
+      'not.contain',
+      'Флюоресцентная булка R2-D3'
+    );
     cy.contains(ingredientCard, 'Флюоресцентная булка R2-D3').within(() => {
-      cy.get('button').click();
-      i = i + 1;
+      cy.contains('button', 'Добавить').click();
+    });
+    cy.get(selectedIngredients).should('contain', 'Флюоресцентная булка R2-D3');
+
+    cy.get(selectedIngredients).should(
+      'not.contain',
+      'Хрустящие минеральные кольца'
+    );
+    cy.contains(ingredientCard, 'Хрустящие минеральные кольца').within(() => {
+      cy.contains('button', 'Добавить').click();
+    });
+    cy.get(selectedIngredients).should(
+      'contain',
+      'Хрустящие минеральные кольца'
+    );
+
+    cy.get(selectedIngredients).should(
+      'not.contain',
+      'Плоды Фалленианского дерева'
+    );
+    cy.contains(ingredientCard, 'Плоды Фалленианского дерева').within(() => {
+      cy.contains('button', 'Добавить').click();
+    });
+    cy.get(selectedIngredients).should(
+      'contain',
+      'Плоды Фалленианского дерева'
+    );
+  });
+
+  it('check order', () => {
+    cy.contains(ingredientCard, 'Флюоресцентная булка R2-D3').within(() => {
+      cy.contains('button', 'Добавить').click();
     });
 
     cy.contains(ingredientCard, 'Хрустящие минеральные кольца').within(() => {
-      cy.get('button').click();
-      i = i + 1;
+      cy.contains('button', 'Добавить').click();
     });
 
     cy.contains(ingredientCard, 'Плоды Фалленианского дерева').within(() => {
-      cy.get('button').click();
-      i = i + 1;
+      cy.contains('button', 'Добавить').click();
     });
 
+    cy.get(modal).should('not.exist');
+    cy.contains('button', 'Оформить заказ').click();
+    cy.wait(5000);
+    cy.get(modal).should('exist');
+    cy.get(modal).find('h2').should('contain.text', '12345');
+    cy.get(modal).find('svg').click();
+    cy.get(modal).should('not.exist');
     cy.get(selectedIngredients)
       .find('.constructor-element')
       .should(($items) => {
-        expect($items).to.have.length(i + 1);
+        expect($items).to.have.length(0);
       });
   });
-  
-  
-  it('check order', () => {
-      cy.contains(ingredientCard, 'Флюоресцентная булка R2-D3').within(() => {
-          cy.get('button').click();
-        });
-        
-        cy.contains(ingredientCard, 'Хрустящие минеральные кольца').within(() => {
-            cy.get('button').click();
-        });
-        
-        cy.contains(ingredientCard, 'Плоды Фалленианского дерева').within(() => {
-            cy.get('button').click();
-        });
-        
-        cy.contains('button', 'Оформить заказ').click();
-        cy.wait(2000);
-        cy.get(modal).should('exist');
-        cy.get(modal).contains('12345').should('have.text', '12345');
-        cy.get(modal).find('svg').click();
-        cy.get(modal).should('not.exist');
-        cy.get(selectedIngredients)
-        .find('.constructor-element')
-        .should(($items) => {
-            expect($items).to.have.length(0);
-        });
-    });
 });
-    
 
 describe('modal', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.contains(ingredientCard, 'Плоды Фалленианского дерева').click();
-  });
+    cy.get(modal).should('not.exist');
+    cy.contains(ingredientCard, 'Хрустящие минеральные кольца').click();
+    });
 
   it('modal exist', () => {
+    cy.wait(2000);
     cy.get(modal).should('exist');
+    cy.get(modal).find('h3').should('contain.text', 'Хрустящие минеральные кольца');
   });
 
   it('close modal', () => {
